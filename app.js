f35e4637f626cb1fefe8f438e47eb5c9575bd60c
RezaFirouzii <sr2000f@gmail.com>
2020-11-01 22:03:28 +0330

    Game setup and first look

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const bird = document.querySelector('.bird');
+    const gameDisplay = document.querySelector('.game-container');
+    const ground = document.querySelector('.ground');
+
+    let birdLeftSide = 220;
+    let birdBottom = 100;
+    let gravity = 2;
+
+});
