090c703bfa76cebd46cdea4ac6558cfae5fd0beb
RezaFirouzii <sr2000f@gmail.com>
2020-11-01 22:20:07 +0330

    Implementing start and jump functions.

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const gameDisplay = document.querySelector('.game-container');
     const ground = document.querySelector('.ground');
 
+    const skyHeight = 580;
+    const skyWidth = 500;
+
     let birdLeftSide = 220;
     let birdBottom = 100;
     let gravity = 2;
 
+    function start() {
+        birdBottom -= gravity;
+        bird.style.bottom = birdBottom + 'px';
+        bird.style.left = birdLeftSide + 'px';
+    }
+
+    function jump() {
+        if (birdBottom < skyHeight - 100) birdBottom += 50;
+        bird.style.bottom = birdBottom + 'px';
+    }
+
+    document.addEventListener('keyup', (event) => {
+        if (event.code === "Space") jump();
+    });
+
+    const fallDown = setInterval(start, 20);
 });
