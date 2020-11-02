3a58575562d7be77a46b99c54b41019717eabac9
RezaFirouzii <sr2000f@gmail.com>
2020-11-02 18:09:53 +0330

    adding obstacles and game over functionality

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const skyHeight = 580;
     const skyWidth = 500;
+    const groundHeight = 150;
+    const groundWidth = 500;
 
     let birdLeftSide = 220;
     let birdBottom = 100;
     let gravity = 2;
+
+    let isOver = false;
 
     function start() {
         birdBottom -= gravity;
@@ -21,9 +25,56 @@ document.addEventListener("DOMContentLoaded", () => {
         bird.style.bottom = birdBottom + 'px';
     }
 
-    document.addEventListener('keyup', (event) => {
+    function control(event) {
         if (event.code === "Space") jump();
-    });
+    }
+    document.addEventListener('keyup', control);
+
+    function generateObstacle() {
+        let obstacleLeftSide = skyWidth;
+        const obstacleBottom = Math.random() * 60;
+        const obstacle = document.createElement('div');
+        obstacle.classList.add('obstacle');
+        gameDisplay.appendChild(obstacle);
+        obstacle.style.left = obstacleLeftSide + 'px';
+        obstacle.style.bottom = obstacleBottom + 'px';
+
+        function moveObstacle() {
+            obstacleLeftSide -= 2;
+            obstacle.style.left = obstacleLeftSide + 'px';
+            if (obstacleLeftSide === -60) {
+                clearInterval(obstacleMove);
+                gameDisplay.removeChild(obstacle);
+            }
+            if (obstacleLeftSide > 200 && obstacleLeftSide < 280 && birdLeftSide === 220
+                || birdBottom === 0) gameOver();
+        }
+        const obstacleMove = setInterval(moveObstacle, 20);
+        setTimeout(generateObstacle, 3000);
+    }
+    generateObstacle();
+
+    function gameOver() {
+        clearInterval(fallDown);
+        isOver = true;
+        document.removeEventListener('keyup', control);
+    }
 
     const fallDown = setInterval(start, 20);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
